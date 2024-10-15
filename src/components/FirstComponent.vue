4890969729d737761d1e4b708b8edaed4a3b9d36
calelx <109821538+calelx@users.noreply.github.com>
2024-10-15 00:03:34 -0600

    primer commit

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Gabriel</h1>
+        <h2>softwarw</h2>
+        <div>
+            <h3>descripcion</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
+                at saepe alias qui doloremque commodi architecto perferendis mollitia
+                nostrum dolorum! Error harum obcaecati eveniet recusandae amet unde quae! Amet, qui.
+            </p>
+        </div>
+    </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import FirstComponentVue from "../components/FirstComponent.vue";
+
+</script>
+<template>
+  <div class="about">
+
+    <FirstComponent />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
